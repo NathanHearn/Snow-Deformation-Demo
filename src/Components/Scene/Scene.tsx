fb1d7ec36bab3da13a1b0f32d2fe626e2cf62da0
nathan.hearn <nathan.hearn@thinkbdw.co.uk>
2025-01-31 15:59:22 +0000

    Render orthographic cam depth buffer to plane texture

--- src/Components/Scene/Scene.tsx
+++ src/Components/Scene/Scene.tsx
@@ -1,14 +1,32 @@
-import { CameraControls, useDepthBuffer } from "@react-three/drei";
+import { CameraControls } from "@react-three/drei";
+import { useDepthBuffer } from "../../Hooks";
+import { useLayoutEffect, useRef } from "react";
+import { Mesh, OrthographicCamera } from "three";
+import { useFrame } from "@react-three/fiber";
 
 export function Scene() {
-  const depthBuffer = useDepthBuffer({ size: 1024 });
+  const orthoCamRef = useRef<OrthographicCamera>(null);
+  const cubeRef = useRef<Mesh>(null);
+  const depthBuffer = useDepthBuffer({
+    size: 1024,
+    cameraRef: orthoCamRef,
+  });
+
+  useLayoutEffect(() => {
+    orthoCamRef.current?.lookAt(0, 0, 0);
+  });
+
+  useFrame(({ clock }) => {
+    if (cubeRef.current) cubeRef.current.rotation.y = clock.getElapsedTime();
+  });
+
   return (
     <>
       {/* Controls */}
       <CameraControls />
 
       {/* Cube */}
-      <mesh>
+      <mesh ref={cubeRef}>
         <boxGeometry args={[1, 1, 1]} />
         <meshBasicMaterial color="hotpink" />
       </mesh>
@@ -16,10 +34,10 @@ export function Scene() {
       {/* Plane */}
       <mesh position={[0, 0, -5]}>
         <planeGeometry args={[8, 8]} />
-        <meshBasicMaterial map={depthBuffer} />
+        <meshBasicMaterial map={depthBuffer.depthTexture} />
       </mesh>
 
-      <orthographicCamera />
+      <orthographicCamera ref={orthoCamRef} position={[0, -1, 0]} />
     </>
   );
 }
