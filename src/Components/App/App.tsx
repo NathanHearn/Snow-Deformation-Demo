c0d5d8be1290de546565461339a1f1504a908f82
nathan.hearn <nathan.hearn@thinkbdw.co.uk>
2025-02-18 21:19:40 +0000

    Add movement controls to ball

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,13 +1,24 @@
 import { Canvas } from "@react-three/fiber";
 import styles from "./App.module.scss";
 import { Scene } from "../Scene";
+import { KeyboardControls } from "@react-three/drei";
 
 function App() {
   return (
     <main className={styles.App}>
-      <Canvas>
-        <Scene />
-      </Canvas>
+      <KeyboardControls
+        map={[
+          { name: "forward", keys: ["ArrowUp", "KeyW"] },
+          { name: "backward", keys: ["ArrowDown", "KeyS"] },
+          { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+          { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+          { name: "jump", keys: ["Space"] },
+        ]}
+      >
+        <Canvas>
+          <Scene />
+        </Canvas>
+      </KeyboardControls>
     </main>
   );
 }
--- src/Components/Scene/Scene.tsx
+++ src/Components/Scene/Scene.tsx
@@ -1,38 +1,19 @@
 import { CameraControls, Environment, Sky } from "@react-three/drei";
 import { Snow } from "../Snow";
-import { Physics, RigidBody } from "@react-three/rapier";
-import { useRef } from "react";
+import { Physics } from "@react-three/rapier";
 import { Box } from "../Box";
+import { Ball } from "../Ball";
 
 export function Scene() {
-  const sphereRef = useRef(null);
-  const cubeRef = useRef(null);
-  const torusRef = useRef(null);
-  const cylinderRef = useRef(null);
-
-  const onBodyClick = (body: any, multiplier: number) => {
-    body.wakeUp();
-    body.applyImpulse({
-      x: (Math.random() - 0.5) * multiplier,
-      y: (Math.random() - 0.5) * multiplier,
-      z: (Math.random() - 0.5) * multiplier,
-    });
-    body.applyTorqueImpulse({
-      x: (Math.random() - 0.5) * multiplier,
-      y: (Math.random() - 0.5) * multiplier,
-      z: (Math.random() - 0.5) * multiplier,
-    });
-  };
-
   return (
     <>
       {/* Controls */}
       <CameraControls
         mouseButtons={{
-          left: 0,
+          left: 1,
           middle: 2,
           wheel: 8,
-          right: 1,
+          right: 0,
         }}
       />
 
@@ -55,45 +36,7 @@ export function Scene() {
 
       {/* Physics */}
       <Physics>
-        <RigidBody colliders="ball" ref={sphereRef}>
-          <mesh
-            position={[-2, 10, -2]}
-            onClick={() => onBodyClick(sphereRef.current, 40)}
-          >
-            <sphereGeometry args={[1.2, 254, 254]} />
-            <meshStandardMaterial color="coral" />
-          </mesh>
-        </RigidBody>
-
-        <RigidBody ref={cubeRef}>
-          <mesh
-            position={[2, 10, -2]}
-            onClick={() => onBodyClick(cubeRef.current, 100)}
-          >
-            <boxGeometry args={[1.5, 4, 1.5]} />
-            <meshStandardMaterial color="darkseagreen" />
-          </mesh>
-        </RigidBody>
-
-        <RigidBody ref={torusRef} colliders="hull">
-          <mesh
-            position={[2, 10, 2]}
-            onClick={() => onBodyClick(torusRef.current, 100)}
-          >
-            <torusGeometry args={[1.5, 0.5, 16, 100]} />
-            <meshStandardMaterial color="dodgerblue" />
-          </mesh>
-        </RigidBody>
-
-        <RigidBody ref={cylinderRef} colliders="hull">
-          <mesh
-            position={[-2, 10, 2]}
-            onClick={() => onBodyClick(cylinderRef.current, 80)}
-          >
-            <cylinderGeometry args={[1, 1, 2]} />
-            <meshStandardMaterial color="violet" />
-          </mesh>
-        </RigidBody>
+        <Ball />
 
         <Box />
       </Physics>
